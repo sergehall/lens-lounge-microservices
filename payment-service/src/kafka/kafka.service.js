36c689359383074f6615cd4eb5cf1c52e897cd8d
serge <serge.hall.dev@gmail.com>
2025-06-03 00:58:37 -0700

    Moved DEFAULT_BANNER_IMAGE_URL declaration below import statements to comply with ESLint. All imports are now correctly placed at the top of the module.

---
+++ payment-service/src/kafka/kafka.service.js
@@ -0,0 +1,40 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.KafkaService = void 0;
+const common_1 = require("@nestjs/common");
+const microservices_1 = require("@nestjs/microservices");
+const common_2 = require("@nestjs/common");
+let KafkaService = class KafkaService {
+    kafkaClient;
+    constructor(kafkaClient) {
+        this.kafkaClient = kafkaClient;
+    }
+    async onModuleInit() {
+        this.kafkaClient.subscribeToResponseOf('order-created');
+        await this.kafkaClient.connect();
+    }
+    async onModuleDestroy() {
+        await this.kafkaClient.close();
+    }
+    emitPaymentCreated(data) {
+        return this.kafkaClient.emit('payment-created', data);
+    }
+};
+exports.KafkaService = KafkaService;
+exports.KafkaService = KafkaService = __decorate([
+    (0, common_1.Injectable)(),
+    __param(0, (0, common_2.Inject)('KAFKA_SERVICE')),
+    __metadata("design:paramtypes", [microservices_1.ClientKafka])
+], KafkaService);

13bd3a2255a7108987fc67b245cf650bcd4770af
serge <serge.hall.dev@gmail.com>
2025-06-13 01:14:03 -0700

    Added shared block

--- shared/src/index.ts
+++ shared/src/index.ts
@@ -4,3 +4,5 @@ export * from './users/dtos/currentUser.dto';
 export * from './users/dtos/guestUsers.dto';
 export * from './users/enums/orgId.enum';
 export * from './users/enums/userRoles.enum';
+export * from './dto/example.dto';
+export * from './events/user-created.event';
